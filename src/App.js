943065609d9fd59858c0dcac9c79fc289556a6d5
mohammedlala <40793220+mohammedlala@users.noreply.github.com>
2023-05-22 07:27:36 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Calendar from './components/Calender';
+
+function App() {
+	//Date Format: MM/DD/YYYY
+	return (
+		<>
+			<Calendar date='12/01/2023' />
+		</>
+	);
+}
+
+export default App;
